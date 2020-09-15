00620a7d88c11fe9f02176a3b73945157a4b15a4
joook <outlook.s@163.com>
2020-09-15 20:47:38 +0800

    Add practice 1-7.

---
+++ 1-7/get_char.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+main()
+{
+    /*
+    int currentChar;
+    while(1)
+    {
+        currentChar = getchar();
+        if(currentChar == EOF)
+        {
+            printf("EOF == %d\n", currentChar);
+            break;
+        }
+    }
+    */
+    printf("EOF == %d\n", EOF);
+}
